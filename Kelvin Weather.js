1c8b4d0783378fc0109b4178ee14508e21e8cd5b
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-04-26 13:47:12 -0400

    Create Kelvin Weather.js

---
+++ Kelvin Weather.js
@@ -0,0 +1,5 @@
+const kelvin = 293; //forecast today
+const celsius = kelvin - 273; //convert to celsius
+let fahrenheit = celsius * (9 / 5) + 32; //calculate fahrenheit
+fahrenheit = Math.floor(fahrenheit); //round down to obtain whole number
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
