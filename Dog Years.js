48fe48aa9d6cbc2a688286e5c8a1b3b123632fdc
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-04-26 14:23:30 -0400

    Create Dog Years.js

---
+++ Dog Years.js
@@ -0,0 +1,8 @@
+//How old would you be if you were a dog?
+//Convert your human years to dog years!    
+const myAge = 22;
+let earlyYears = 2 * 10.5;
+let laterYears = (myAge - 2) * 4;
+const myName = 'Jeannie'.toLowerCase()
+const myAgeInDogYears = earlyYears + laterYears;
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
