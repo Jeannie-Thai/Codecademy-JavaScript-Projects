64d85bc8ed58105a5712dff8986b5b3e5c4a706b
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-04-26 14:01:55 -0400

    Update Kelvin Weather.js

--- Kelvin Weather.js
+++ Kelvin Weather.js
@@ -1,3 +1,4 @@
+// convert kelvin to fahrenheit
 const kelvin = 293; //forecast today
 const celsius = kelvin - 273; //convert to celsius
 let fahrenheit = celsius * (9 / 5) + 32; //calculate fahrenheit
