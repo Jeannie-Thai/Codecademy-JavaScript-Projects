b7359487aa883f124ad7c4ece31d407db58f2113
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-04-30 12:14:04 -0400

    Update Secret Message.js

--- Secret Message.js
+++ Secret Message.js
@@ -1,3 +1,4 @@
+//Transform an array of strings into a secret message!
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
 
 //console.log(secretMessage.length);
