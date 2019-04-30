7dec825c69da251cf84f2af82bef5053534d14a6
Jeannie Thai <50002031+Jeannie-Thai@users.noreply.github.com>
2019-04-30 10:12:58 -0400

    Create Secret Message.js

---
+++ Secret Message.js
@@ -0,0 +1,23 @@
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+//console.log(secretMessage.length);
+secretMessage.pop(); //remove string Javascript
+//console.log(secretMessage);
+//console.log(secretMessage.length);
+
+secretMessage.push('to','Program') //add two elements 
+
+secretMessage[7] = 'right'; //replace 7th element, easily
+//console.log(secretMessage);
+
+secretMessage.shift(); //remove first string of the array
+//console.log(secretMessage);
+
+secretMessage.unshift('Programming'); //add first string
+//console.log(secretMessage);
+
+secretMessage.splice(6, 5,'know,'); //remove strings from get to time and replace with single string know
+//console.log(secretMessage);
+
+console.log(secretMessage.join(' ')); //print as a sentence
+
